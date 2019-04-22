cea753fe0ea4a2b230adf19bed4aa3f935ea5e54
ken1286 <kindle1286@gmail.com>
2019-04-22 13:32:10 -0700

    Header complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+function HeaderTitle() {
+  return (
+    <div className="header-title">
+      <h2>Lamdba School</h2>
+      <p>@LambdaSchoool 26 jan</p>
+    </div>
+  )
+}
+
+export default HeaderTitle;
