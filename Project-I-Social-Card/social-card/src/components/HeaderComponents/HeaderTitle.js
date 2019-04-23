20597d9cc63357b9ccef59bee7bc8935fe453188
ken1286 <kindle1286@gmail.com>
2019-04-22 22:27:37 -0700

    Date added using moment js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,14 +1,18 @@
 import React from 'react';
+// import Moment from 'moment';
 import './Header.css';
 
 
 function HeaderTitle() {
+  let date = require('moment');
+  
   return (
+    
     <div className="header-title">
       <h2>Lamdba School</h2>
-      <p>@LambdaSchoool 26 jan</p>
+      <p>@LambdaSchool · {date().format("DD MMM").toLowerCase()}</p>
     </div>
-  )
+  );
 }
 
 export default HeaderTitle;
