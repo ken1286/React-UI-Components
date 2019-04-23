9380428e8eb5e0ddd576a6a08276a36cbf7a9ea9
ken1286 <kindle1286@gmail.com>
2019-04-23 15:08:40 -0700

    Calculator frame finished

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Button.css';
+import ActionButton from './ActionButton';
+import NumberButton from './NumberButton';
+
+let numbers = [{num:'1'}, {num:'2'}, {num:'3'}, {num:'4'}, {num:'5'}, {num:'6'}, {num:'7'}, {num:'8'}, {num:'9'}]
+
+let symbols = [{symbol:'/'}, {symbol:'X'}, {symbol:'-'}, {symbol:'+'}, {symbol:'='}];
+
+function ButtonContainer() {
+  return (
+  <div className="button-container">
+    <div className="white-buttons">
+
+      <ActionButton text="clear" buttonStyle="clear" />
+
+      <div className="numbers">
+      {numbers.map( element => {
+        return <NumberButton buttonStyle='number-button' text={element.num} />
+      })};
+      </div>
+
+      <ActionButton text="0" buttonStyle="clear" />
+
+    </div>
+
+    <div className="symbols">
+      {symbols.map( element => {
+        return <NumberButton buttonStyle='symbol-button' text={element.symbol} />
+      })};
+    </div>
+    
+  </div>
+  );
+}
+
+export default ButtonContainer;
